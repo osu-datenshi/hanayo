d7ece6577e0a4d64704a73fdd39ab7268a1140b7
unknown <jame_glove@yahoo.com>
2021-01-01 08:15:54 +0800

    block ipv6 test 1.2

--- funcmap.go
+++ funcmap.go
@@ -514,7 +514,8 @@ var logoColours = [...]string{
 	"red",
 }
 
-func BlockIp() func(c *gin.Context) {
+func BlockIp(), func(c *gin.Context) {
+	return func(c *gin.Context) {
 	if strings.Contains(c.ClientIP(), ":") {
 		c.String(http.StatusForbidden, "IPV6 not allowed!")
 		c.Abort()
