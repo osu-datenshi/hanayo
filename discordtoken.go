b98a41865676d60d4f1d2a820551a02ddf2520cd
unknown <jame_glove@yahoo.com>
2021-02-27 06:40:19 +0800

    token check

--- discordtoken.go
+++ discordtoken.go
@@ -12,13 +12,26 @@ func DiscordGenToken(c *gin.Context) {
 		return
 	}
 
-	db.Exec("DELETE FROM discord_tokens WHERE userid = ?", ctx.User.ID)
+	//db.Exec("DELETE FROM discord_tokens WHERE userid = ?", ctx.User.ID)
 
-	key := rs.String(32)
+	var (
+		Token     string      `json:"token"`
+		Userid    int         `json:"userid"`
+		RoleID    interface{} `json:"role_id"`
+		Verified  int         `json:"verified"`
+		DiscordID interface{} `json:"discord_id"`
+	)
 
-	db.Exec("INSERT INTO discord_tokens(userid, token) VALUES (?, ?)", ctx.User.ID, key)
-
-	simple(c, getSimple("/discordtokens"), []message{successMessage{
-		T(c, "Your new Discord token is <code>%s</code>. Do not use this if you already verify it.", key),
-	}}, nil)
+	err := db.QueryRow("SELECT token, userid, role_id, verified, discord_id FROM discord_tokens WHERE verified = 1 AND userid = ?", ctx.User.ID).Scan(&Token, &Userid, &RoleID, &Verified, &DiscordID)
+	if err != nill {
+		key := rs.String(32)
+		db.Exec("INSERT INTO discord_tokens(userid, token) VALUES (?, ?)", ctx.User.ID, key)
+		simple(c, getSimple("/discordtokens"), []message{successMessage{
+			T(c, "Your new Discord token is <code>%s</code>. Do not use this if you already verify it.", key),
+		}}, nil)
+	} else {
+		simple(c, getSimple("/discordtokens"), []message{errorMessage{
+			T(c, "You are already verified!"),
+		}}, nil)
+	}
 }
