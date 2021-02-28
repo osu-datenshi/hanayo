a49a74cf686950192bba4fc9bece321a865cab77
unknown <jame_glove@yahoo.com>
2021-02-28 12:10:44 +0800

    fix bodo

--- discordtoken.go
+++ discordtoken.go
@@ -33,11 +33,11 @@ func CheckDCToken(c *gin.Context) {
 		DiscordID interface{}
 	)
 
-    d, err := db.QueryRow("SELECT token, userid, role_id, verified, discord_id FROM discord_tokens WHERE userid = ? AND verified = 1 LIMIT 1", ctx.User.ID).Scan(&Token, &Userid, &RoleID, &Verified, &DiscordID)
+    err := db.Get("SELECT token, userid, role_id, verified, discord_id FROM discord_tokens WHERE userid = ? AND verified = 1 LIMIT 1", ctx.User.ID).Scan(&Token, &Userid, &RoleID, &Verified, &DiscordID)
 
 	if err != nil {
 		simple(c, getSimpleByFilename("discordblock.html"), nil, map[string]interface{}{
-			"DiscordID": d["discord_id"],
+			"DiscordID": "discord_id",
 		})
         } else {
 		DiscordGenToken(c)
