ec61b6241fdcfb791be8171cfe0431954bf68e7f
unknown <jame_glove@yahoo.com>
2021-05-02 12:09:17 +0800

    update: routing ke page

--- block.go
+++ block.go
@@ -9,7 +9,7 @@ import (
 func BlockIp() func(c *gin.Context) {
 	return func(c *gin.Context) {
 		if strings.Contains(c.ClientIP(), ":") {
-			c.Redirect(http.StatusMovedPermanently, "https://raw.githubusercontent.com/osu-datenshi/assets/master/fuck-ipv6.jpg")
+			c.Redirect(302, "/blockedipv6")
 		}
 	}
 }
